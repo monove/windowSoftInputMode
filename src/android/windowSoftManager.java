4ebf8922bf4371a1ecc515cc13a487268f585b7e
Magnus Arinell <magnus.arinell@afconsult.com>
2017-03-20 12:05:19 +0100

    Fixed bug

--- src/android/windowSoftManager.java
+++ src/android/windowSoftManager.java
@@ -39,7 +39,7 @@ public windowSoftManager() {}
         cordova.getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
       else if(action.equals("adjustResize"))
         cordova.getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
-	  else if(action.equals("adjustResize|stateVisible|stateAlwaysHidden")
+	  else if(action.equals("adjustResize|stateVisible|stateAlwaysHidden"))
 		cordova.getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE | WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE | WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
   });
